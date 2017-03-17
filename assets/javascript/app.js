baa323ef7196d25c7393d5ea4a9b9a670601358d
Mike Bass <Mike Bass>
2017-03-17 13:09:36 -0400

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -46,60 +46,6 @@
 				app.appendTableRow( childSnapshot.val() );
 			} );
 
-			var jumperData = '',
-        name = '',
-        destination = '',
-        firstJumper = '',
-        frequency = '';
-
-  	jumpers.on("child_added", function (childSnapshot) {
-    jumperData = childSnapshot.val(),
-    name = jumperData.name,
-    destination = jumperData.destination,
-    firstJumper = jumperData.first_jumper,
-    frequency = jumperData.frequency;
-    
-    buildSchedule(name, destination, firstJumper, frequency);
-    
-  }, errData);
-  function errData(data) {
-    console.log(data);
-  }
-
-  function buildSchedule(name, destination, firstJumper, frequency) {
-    var newRow = $("<tr>"),
-        mdlCellClass = "mdl-data-table__cell--non-numeric",
-        nextJumper = getNextJumper(firstJumper, frequency);
-
-    console.log(getNextJumper(firstJumper, frequency));
-
-			function getNextJumper(start, interval)
-			{
-				var now = moment(),
-				hours= start.substr
-			}
-
-			function getNextJumper(start, interval) {
-    var now = moment(),
-        hours = start.substr(0, 2),
-        minutes = start.substr(3, 4),
-        jumpers = moment().startOf('day').hour(parseInt(hours)).minute(parseInt(minutes)),
-        duration = moment.duration(now.diff(jumpers)).asMinutes(),
-        minutesUntil = duration % interval,
-        nextJumper = interval - minutesUntil,
-        newJumper = now.add(nextJumper, "minutes").format("HH:MM");
-    
-			    return moment(now).format("h:mm a"); 
-			  }
- 				console.log(newJumper); 
-
-			  function resetFields() {
-			    $("#jumper-name").val("");
-			    $("#destination").val("");
-			    $("#first-jumper").val("");
-			    $("#frequency").val("");
-			  }
-
 			$( "#submit-btn" ).click(
 
 				function () {
@@ -114,8 +60,6 @@
 						rate: rate,
 						dateAdded: firebase.database.ServerValue.TIMESTAMP
 					} );
-
-					resetFields();
 				}
 			);
 
@@ -134,4 +78,4 @@
 
 
 
-};
+}() );
